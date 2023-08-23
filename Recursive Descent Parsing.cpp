df4d6f0ef09496041fb6198b5bec6ed73c65d59c
JambuluruNavyaSree <113337544+JambuluruNavyaSree@users.noreply.github.com>
2023-08-23 10:33:14 +0530

    Update Recursive Descent Parsing.cpp

--- Recursive Descent Parsing.cpp
+++ Recursive Descent Parsing.cpp
@@ -1,64 +1,129 @@
-#include<stdio.h>
+                                                                                                                                                                      #include<stdio.h>
 #include<string.h>
-#include<ctype.h> 
-char input[10];
-int i,error;
-void E();
-void T();
-void Eprime();
-void Tprime();
-void F(); 
-main()
+int E(),Edash(),T(),Tdash(),F();
+char *ip;
+char string[50];
+int main()
 {
-i=0;
-error=0;
-printf("Enter an arithmetic expression   :  ");
-gets(input);
-E();
-if(strlen(input)==i&&error==0)
-    printf("\nAccepted..!!!\n");
-else 
-	printf("\nRejected..!!!\n");
-}         
-void E()
-{
-     T();
-     Eprime();
+printf("Enter the string\n");
+scanf("%s",string);
+ip=string;
+printf("\n\nInput\tAction\n--------------------------------\n");
+ 
+if(E() && ip=="\0"){
+printf("\n--------------------------------\n");
+printf("\n String is successfully parsed\n");
 }
-void Eprime()
-{
-     if(input[i]=='+')
-     {
-     i++;
-     T();
-     Eprime();
-     }
+else{
+printf("\n--------------------------------\n");
+printf("Error in parsing String\n");
 }
-void T()
-{
-     F();
-     Tprime();
 }
-void Tprime()
+int E()
 {
-    if(input[i]=='*')
-    {
-        i++;
-        F();
-        Tprime();
-    }
+printf("%s\tE->TE' \n",ip);
+if(T())
+{
+if(Edash())
+{
+return 1;
 }
-void F()
+else
+return 0;
+}
+else
+return 0;
+}
+int Edash()
 {
-if(isalnum(input[i]))
-	i++;
-else if(input[i]=='(')
+if(*ip=='+')
 {
-    i++;
-    E();
-    if(input[i]==')')
-    i++;
-    else error=1;
-}        
-else error=1;
+printf("%s\tE'->+TE' \n",ip);
+ip++;
+if(T())
+{
+if(Edash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+else
+{
+printf("%s\tE'->^ \n",ip);
+return 1;
+}
+}
+int T()
+{
+printf("%s\tT->FT' \n",ip);
+if(F())
+{
+ 
+if(Tdash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+int Tdash()
+{
+if(*ip=='*')
+{
+printf("%s\tT'->*FT' \n",ip);
+ip++;
+if(F())
+{
+if(Tdash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+else
+{
+printf("%s\tT'->^ \n",ip);
+return 1;
+}
+}
+int F()
+{
+if(*ip=='(')
+{
+printf("%s\tF->(E) \n",ip);
+ip++;
+if(E())
+{
+if(*ip==')')
+{
+ip++;
+return 0;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+ 
+else if(*ip=='i')
+{
+ip++;
+printf("%s\tF->id \n",ip);
+return 1;
+}
+else
+return 0;
 }
